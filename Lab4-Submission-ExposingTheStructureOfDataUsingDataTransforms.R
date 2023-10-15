5632e0122c8a137a248861423699b7a896d35bfe
git-YannIck1998 <yannagre1997@gmail.com>
2023-10-15 03:15:03 +0300

    Submission file created and dataset uploaded

---
+++ Lab4-Submission-ExposingTheStructureOfDataUsingDataTransforms.R
@@ -0,0 +1,75 @@
+if (require("languageserver")) {
+  require("languageserver")
+} else {
+  install.packages("languageserver", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+
+library(readr)
+StudentPerformanceDataset <- read.csv("data/StudentPerformanceDataset.csv")
+View(StudentPerformanceDataset)
+
+# STEP 1 - Required Packages
+## mlbench ----
+if (require("mlbench")) {
+  require("mlbench")
+} else {
+  install.packages("mlbench", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+
+## readr ----
+if (require("readr")) {
+  require("readr")
+} else {
+  install.packages("readr", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+
+## caret ----
+if (require("caret")) {
+  require("caret")
+} else {
+  install.packages("caret", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+
+## e1071 ----
+if (require("e1071")) {
+  require("e1071")
+} else {
+  install.packages("e1071", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+
+## factoextra ----
+if (require("factoextra")) {
+  require("factoextra")
+} else {
+  install.packages("factoextra", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+
+## FactoMineR ----
+if (require("FactoMineR")) {
+  require("FactoMineR")
+} else {
+  install.packages("FactoMineR", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+
+## dplyr ----
+if (!is.element("dplyr", installed.packages()[, 1])) {
+  install.packages("dplyr", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+require("dplyr")
+
+#STEP 2 - Load the dataset
+##data(StudentPerformanceDataset)
+names()
+
+stdperf <- StudentPerformanceDataset %>%
+  select(class_group, YOB, `Quizzes and Bonus Marks`, `LabWork`, `CAT 1`, `CAT 2`, Coursework, EXAM, TOTAL)
+
+summary(stdperf)
