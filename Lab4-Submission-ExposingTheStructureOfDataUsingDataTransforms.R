401820ebe01310a0cf90590e903c8a5727e689b1
Natasha Gichira <71381844+NatashaGichira@users.noreply.github.com>
2023-10-16 09:40:20 +0300

    Update Lab4-Submission-ExposingTheStructureOfDataUsingDataTransforms.R
    Signed-off-by: Natasha Gichira  <71381844+NatashaGichira@users.noreply.github.com>

--- Lab4-Submission-ExposingTheStructureOfDataUsingDataTransforms.R
+++ Lab4-Submission-ExposingTheStructureOfDataUsingDataTransforms.R
@@ -5,9 +5,12 @@ if (require("languageserver")) {
                    repos = "https://cloud.r-project.org")
 }
 
+<<<<<<< HEAD
+=======
 library(readr)
 StudentPerformanceDataset <- read.csv("data/StudentPerformanceDataset.csv")
 View(StudentPerformanceDataset)
+>>>>>>> main
 
 # STEP 1 - Required Packages
 ## mlbench ----
@@ -66,6 +69,145 @@ if (!is.element("dplyr", installed.packages()[, 1])) {
 require("dplyr")
 
 #STEP 2 - Load the dataset
+<<<<<<< HEAD
+
+StudentPerformanceDataset <- read.csv("data/BI1StudentPerformanceDataset.csv")
+View(StudentPerformanceDataset)
+dim(StudentPerformanceDataset)
+sapply(StudentPerformanceDataset, class)
+
+variable_to_find <- c("YOB", 
+                      "Coursework TOTAL: x/40 (40%)", 
+                      "EXAM: x/60 (60%)",
+                      "TOTAL = Coursework TOTAL + EXAM (100%)")
+
+column_indices <- which(colnames(StudentPerformanceDataset) %in% variable_to_find)
+
+# STEP 3 - Scale Data Transform
+
+summary(StudentPerformanceDataset)
+
+birth <- as.numeric(unlist(StudentPerformanceDataset[, 3]))
+course <- as.numeric(unlist(StudentPerformanceDataset[, 97]))
+exam <- as.numeric(unlist(StudentPerformanceDataset[, 98]))
+total <- as.numeric(unlist(StudentPerformanceDataset[, 99]))
+
+hist(birth, main = names(StudentPerformanceDataset)[3])
+hist(course, main = names(StudentPerformanceDataset)[97])
+hist(exam, main = names(StudentPerformanceDataset)[98])
+hist(total, main = names(StudentPerformanceDataset)[99])
+
+model_of_the_transform <- preProcess(StudentPerformanceDataset, method = c("scale"))
+print(model_of_the_transform)
+stdperf_data_scale_transform <- predict(model_of_the_transform, StudentPerformanceDataset)
+
+summary(stdperf_data_scale_transform)
+
+StudentPerformanceDataset_birth <- as.numeric(unlist(stdperf_data_scale_transform[, 3]))
+hist(StudentPerformanceDataset_birth, main = names(stdperf_data_scale_transform)[3])
+
+# STEP 4 - Apply Centre Basic Transform
+
+summary(StudentPerformanceDataset)
+model_of_the_transform <- preProcess(StudentPerformanceDataset, method = c("center"))
+print(model_of_the_transform)
+stdperf_data_center_transform <- predict(model_of_the_transform, StudentPerformanceDataset)
+summary(stdperf_data_center_transform)
+
+# STEP 5 - Apply a Standardize Data Transform
+
+summary(StudentPerformanceDataset)
+sapply(StudentPerformanceDataset[3], sd)
+model_of_the_transform <- preProcess(StudentPerformanceDataset,
+                                     method = c("scale", "center"))
+print(model_of_the_transform)
+stdperf_data_standardize_transform <- predict(model_of_the_transform, StudentPerformanceDataset) # nolint
+
+summary(stdperf_data_standardize_transform)
+sapply(stdperf_data_standardize_transform[3], sd)
+
+# STEP 6 - Apply a Normalize Data Transform
+
+summary(StudentPerformanceDataset)
+model_of_the_transform <- preProcess(StudentPerformanceDataset, method = c("range"))
+print(model_of_the_transform)
+stdperf_data_normalize_transform <- predict(model_of_the_transform, StudentPerformanceDataset)
+summary(stdperf_data_normalize_transform)
+
+# STEP 7 - Apply a Box-Cox Power Transform
+
+summary(stdperf_data_standardize_transform)
+
+# Calculate the skewness before the Box-Cox transform
+sapply(stdperf_data_standardize_transform[3],  skewness, type = 2)
+sapply(stdperf_data_standardize_transform[3], sd)
+
+model_of_the_transform <- preProcess(stdperf_data_standardize_transform,
+                                     method = c("BoxCox"))
+print(model_of_the_transform)
+stdperf_data_box_cox_transform <- predict(model_of_the_transform,
+                                       stdperf_data_standardize_transform)
+
+summary(stdperf_data_box_cox_transform)
+
+sapply(stdperf_data_box_cox_transform[3],  skewness, type = 2)
+sapply(stdperf_data_box_cox_transform[3], sd)
+
+# Calculate the skewness after the Box-Cox transform
+sapply(stdperf_data_box_cox_transform[3],  skewness, type = 2)
+sapply(stdperf_data_box_cox_transform[3], sd)
+
+# STEP 8 - Apply a Yeo-Johnson Power Transform
+
+summary(stdperf_data_standardize_transform)
+
+# Calculate the skewness before the Yeo-Johnson transform
+sapply(stdperf_data_standardize_transform[3],  skewness, type = 2)
+sapply(stdperf_data_standardize_transform[3], sd)
+
+model_of_the_transform <- preProcess(stdperf_data_standardize_transform,
+                                     method = c("YeoJohnson"))
+print(model_of_the_transform)
+stdperf_data_yeo_johnson_transform <- predict(model_of_the_transform, # nolint
+                                           stdperf_data_standardize_transform)
+
+# AFTER
+summary(stdperf_data_yeo_johnson_transform)
+
+# Calculate the skewness after the Yeo-Johnson transform
+sapply(stdperf_data_yeo_johnson_transform[3],  skewness, type = 2)
+sapply(stdperf_data_yeo_johnson_transform[3], sd)
+
+# STEP 9.a - PCA Linear Algebra Transform for Dimensionality Reduction
+
+summary(StudentPerformanceDataset)
+
+model_of_the_transform <- preProcess(StudentPerformanceDataset, method =
+                                       c("scale", "center", "pca"))
+print(model_of_the_transform)
+stdperf_pca_dr <- predict(model_of_the_transform, StudentPerformanceDataset)
+
+summary(stdperf_pca_dr)
+dim(stdperf_pca_dr)
+
+# STEP 10 - ICA Linear Algebra Transform for Dimensionality Reduction
+
+if (!is.element("fastICA", installed.packages()[, 1])) {
+  install.packages("fastICA", dependencies = TRUE)
+}
+require("fastICA")
+
+summary(StudentPerformanceDataset)
+
+model_of_the_transform <- preProcess(StudentPerformanceDataset,
+                                     method = c("scale", "center", "ica"),
+                                     n.comp = 8)
+print(model_of_the_transform)
+stdperf_ica_dr <- predict(model_of_the_transform, StudentPerformanceDataset)
+
+summary(stdperf_ica_dr)
+
+=======
 ##data(StudentPerformanceDataset)
 names()
 
@@ -73,3 +215,4 @@ stdperf <- StudentPerformanceDataset %>%
   select(class_group, YOB, `Quizzes and Bonus Marks`, `LabWork`, `CAT 1`, `CAT 2`, Coursework, EXAM, TOTAL)
 
 summary(stdperf)
+>>>>>>> main
